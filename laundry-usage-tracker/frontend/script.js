265d1aa404c1a5391e608a33e9bfe7813cffe39a
harishchandraguntupalli98 <harishchandraguntupalli98@gmail.com>
2026-02-11 14:57:54 -0600

    Convert laundry project from submodule to regular folder

---
+++ laundry-usage-tracker/frontend/script.js
@@ -0,0 +1,65 @@
+const API_BASE = "https://laundry-usage-tracker.onrender.com/api";
+
+console.log("✅ script.js loaded");
+""
+function loadStatus() {
+  fetch(`${API_BASE}/status`)
+    .then(res => res.json())
+    .then(data => {
+      data.forEach(machine => {
+        const statusText = machine.inUse
+          ? `In use by ${machine.usedBy}`
+          : "Available";
+
+        if (machine.machine === "WASHER") {
+          document.getElementById("washerStatus").innerText = statusText;
+        } else if (machine.machine === "DRYER") {
+          document.getElementById("dryerStatus").innerText = statusText;
+        }
+      });
+    });
+}
+
+function useMachine(machine) {
+  const nameInput =
+    machine === "WASHER"
+      ? document.getElementById("washerName")
+      : document.getElementById("dryerName");
+
+  fetch(`${API_BASE}/use`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      machine: machine,
+      name: nameInput.value
+    })
+  })
+    .then(res => res.text())
+    .then(msg => {
+      alert(msg);
+      loadStatus();
+    });
+}
+
+function releaseMachine(machine) {
+  fetch(`${API_BASE}/release`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ machine: machine })
+  })
+    .then(res => res.text())
+    .then(msg => {
+      alert(msg);
+      loadStatus();
+    });
+
+    window.addEventListener("click", function playLaughOnce() {
+      const audio = document.getElementById("laughSound");
+      audio.play().catch(err => console.log("Autoplay blocked:", err));
+  
+      // Remove listener so it only plays once
+      window.removeEventListener("click", playLaughOnce);
+  });
+}
+
+loadStatus();
